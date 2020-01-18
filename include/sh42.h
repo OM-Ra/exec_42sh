3660fc20363e97dde19cf5551f727c9d272e7746
ra <ra@localhost.localdomain>
2020-01-18 20:38:00 +0300

    добалена структура для перенаправления потоков и новые функции

--- include/sh42.h
+++ include/sh42.h
@@ -15,15 +15,31 @@
 #include <sys/wait.h>
 #include <sys/stat.h>
 
+int exec_status;
+// структура для перенаправления потоков
+typedef struct			s_red_stream
+{
+	int					stream_a;						// какой поток будет перенаправлен
+	int					stream_in;						// в какой поток будет перенаправлен
+	char				stream_name[BUFSIZ];			// в какой файл будет направлен поток
+
+	struct s_red_stream	*next;
+}						t_red_stream;
 
 typedef struct			s_pars_list
 {
-	char				name_run_func[BUFSIZ];			// имя запускаемой программы
+	char				name_func[BUFSIZ];				// имя запускаемой программы
+	char				name_run_func[BUFSIZ];			// имя запускаемой программы с путём запуска
 	char				**pars_args;					// разбитые аргументы строки 
 	int					status;							// статус завершения вызванной программы (заполню)
 
 	int					flag_file;						// флаг файла: ">" = "1", ">>" = "2", "<" = "-1"
 	char				name_file[BUFSIZ];				// имя запускаемого файла либо амперсанд с номером дескриптора: &fd
+
+	t_red_stream		*stream_list;					// для перенаправления потоков
+
+	int					echo_status;					// флаг заполнения статуса
+	char				str_status[BUFSIZ][BUFSIZ];		// строки в которых нужно вставить статус
 
 	int					flag_ampersant;					// флаг "&"
 	int					nbr_ampersant;					// номер фонового режима
@@ -41,10 +57,13 @@ typedef struct			s_pars_list
 void					check_choice(t_pars_list **list);
 int						check_run(t_pars_list **list);
 int						create_file(t_pars_list *list);
-void					dup_fd_and_close(int fd, int dup_fd);
+int						dup_fd_and_close(int fd, int dup_fd);
 int						run_ampersant(t_pars_list **list);
 void					run_exec(int fd, t_pars_list *list);
 int						run_pipe(int fd_stdin, t_pars_list **list);
+int						new_or_open_file(char *file_name, int flag_open);
+int						redirect_stream(t_red_stream *stream_list);
+
 int						main(void);
 
 /***
