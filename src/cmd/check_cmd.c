c542f77a882fa71c5a7da73c12b6cbe59536e275
ra <ra@localhost.localdomain>
2020-02-07 14:19:23 +0300

    аргумент функции изменён на строку

--- src/cmd/check_cmd.c
+++ src/cmd/check_cmd.c
@@ -6,29 +6,29 @@ static void write_character(char *buf, char sim)
 	buf[1] = '\0';
 }
 // проверяет наличие внутренних команд в листе
-static int	check_name_cmd(t_pars_list *list)
+static int	check_name_cmd(char *name_func)
 {
-	if (!(ft_strcmp("true", list->name_func)) ||
-		!(ft_strcmp("false", list->name_func)) ||
-		!(ft_strcmp("cd", list->name_func)) ||
-		!(ft_strcmp("echo", list->name_func)) ||
-		!(ft_strcmp("pwd", list->name_func)) ||
-		!(ft_strcmp("setenv", list->name_func)) ||
-		!(ft_strcmp("unsetenv", list->name_func)) ||
-		!(ft_strcmp("env", list->name_func)))
+	if (!(ft_strcmp("true", name_func)) ||
+		!(ft_strcmp("false", name_func)) ||
+		!(ft_strcmp("cd", name_func)) ||
+		!(ft_strcmp("echo", name_func)) ||
+		!(ft_strcmp("pwd", name_func)) ||
+		!(ft_strcmp("setenv", name_func)) ||
+		!(ft_strcmp("unsetenv", name_func)) ||
+		!(ft_strcmp("env", name_func)))
 		return (1);
 	return (0);
 }
 // проверяет наличие первого символа внутренних команд в листе
-int			check_cmd(t_pars_list *list)
+int			check_cmd(char *name_func)
 {
 	char buf[2];
 
-	write_character(buf, list->name_func[0]);
+	write_character(buf, name_func[0]);
 	if (!(ft_strcmp("t", buf)) || !(ft_strcmp("f", buf)) ||
 		!(ft_strcmp("c", buf)) || !(ft_strcmp("e", buf)) ||
 		!(ft_strcmp("p", buf)) || !(ft_strcmp("s", buf)) ||
 		!(ft_strcmp("u", buf))) // true, false, cd, echo, pwd, setenv, unsetenv, env
-		return (check_name_cmd(list));
+		return (check_name_cmd(name_func));
 	return (0);
 }
