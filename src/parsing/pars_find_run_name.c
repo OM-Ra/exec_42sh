17e73da40fcb4fd03803eeee2dc3edbd03843c46
ra <ra@localhost.localdomain>
2020-02-07 14:20:23 +0300

    функции для записи в строку вывода команды

--- src/parsing/pars_find_run_name.c
+++ src/parsing/pars_find_run_name.c
@@ -1,4 +1,56 @@
-//
-// Created by ra on 07.02.2020.
-//
+#include "sh42.h"
+
+static void	error_run_exec(char *name_cmd)
+{
+	ft_putstr_fd("42sh: command not found: ", 2);
+	ft_putstr_fd(name_cmd, 2);
+	ft_putchar_fd('\n', 2);
+	exit(127);
+}
+// вывод об ошибке если доступ к запуску команды не разрешён
+static void error_access_exec(char *name_cmd)
+{
+	ft_putstr_fd("42sh: command not access: ", 2);
+	ft_putstr_fd(name_cmd, 2);
+	ft_putchar_fd('\n', 2);
+	exit(126);
+}
+// записывает адрес и имя функции для запуска
+static void	find_name_path(char *name_path, size_t *i, char *name_cmd, char *buf_name_cmd)
+{
+	size_t j;
+
+	j = 0;
+	while (name_path[*i])
+	{
+		if (name_path[*i] == ':')
+		{
+			(*i)++;
+			break ;
+		}
+		buf_name_cmd[j++] = name_path[(*i)++];
+	}
+	buf_name_cmd[j] = '\0';
+	ft_strcat(buf_name_cmd, "/");
+	ft_strcat(buf_name_cmd, name_cmd);
+}
+// запись имени функции с путём запуска
+void		pars_find_run_name(t_exec execlist, char *name_cmd, char *buf_name_cmd)
+{
+	size_t	i;
+
+	i = 0;
+	while (execlist.exec_envlist.path[i])
+	{
+		find_name_path(execlist.exec_envlist.path, &i, name_cmd, buf_name_cmd);
+		if (!access(buf_name_cmd, 0))
+		{
+			if (!access(buf_name_cmd, 1))
+				return ;
+			else
+				error_access_exec(name_cmd);
+		}
+	}
+	error_run_exec(name_cmd);
+}
 
--- src/parsing/pars_run_cmd.c
+++ src/parsing/pars_run_cmd.c
@@ -1,4 +1,51 @@
-//
-// Created by ra on 07.02.2020.
-//
+#include "sh42.h"
 
+static int 	cod_parent(int pid, char *str, int *pfd)
+{
+	size_t	len;
+	char 	buf[BUFSIZ];
+	int		status;
+
+	close(pfd[1]);
+	while ((len = read(pfd[0], buf, (BUFSIZ - 1))))
+	{
+		buf[len] = '\0';
+		ft_strcat(str, buf);
+	}
+	close(pfd[0]);
+	waitpid(pid, &status, WUNTRACED);
+	return (status);
+}
+
+static void		cod_chuild(t_exec execlist, char **arg_cmd, int *pfd)
+{
+	extern char	**environ;	///// заменить на массив из execlist
+	t_pars_list	list;
+	char 		buf_name_cmd[BUFSIZ];
+
+	dup_fd_and_close(STDOUT_FILENO, pfd[1]);
+	dup_fd_and_close(STDERR_FILENO, pfd[1]);
+	pars_find_run_name(execlist, arg_cmd[0], buf_name_cmd);
+	close(pfd[0]);
+	if (check_cmd(arg_cmd[0]))
+	{
+		ft_strcat(list.name_func, arg_cmd[0]);
+		list.status = 0;
+		run_cmd(&list);
+		exit(list.status);
+	}
+	execve(buf_name_cmd, arg_cmd, environ);
+}
+
+int 			pars_run_cmd(t_exec execlist, char **arg_cmd, char *str)
+{
+	pid_t	pid;
+	int 	pfd[2];
+
+	pipe(pfd);
+	if ((pid = fork()) < 0)
+		error_system(EXEC_ERROR_NUM);
+	if (!pid)
+		cod_chuild(execlist, arg_cmd, pfd);
+	return (cod_parent(pid, str, pfd));
+}
